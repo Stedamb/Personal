415d3b2c658c66d915ade4047807838528dff2ef
Stefano <stedamb@gmail.com>
2022-08-11 21:56:58 +0200

    asas

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ workBox.onclick = function() {
     workBox.style.backgroundImage = "url('./res/github.png')";
     workBox.style.backgroundSize = "500px";
     workBox.style.backgroundBlendMode = "normal";
-    setTimeout(() => {  location.href = "http://github.com/Stedamb"; }, 1000);
+    setTimeout(() => {  
+        location.href = "http://github.com/Stedamb"; 
+        workBox.style.backgroundImage = "url('./res/coding.gif')";
+        workBox.style.backgroundSize = "auto 400px";
+        workBox.style.backgroundBlendMode = "darken";
+    }, 1000);
 }
 
 socialBox.onclick = function() {
@@ -16,7 +21,13 @@ socialBox.onclick = function() {
     socialBox.style.backgroundImage = "url('./res/insta.png')";
     socialBox.style.backgroundSize = "500px";
     socialBox.style.backgroundBlendMode = "normal";
-    setTimeout(() => {  location.href = "https://www.instagram.com/ste_dambrosio/"; }, 1000);
+    setTimeout(() => {  
+        location.href = "https://www.instagram.com/ste_dambrosio/";
+        socialBox.style.backgroundImage = "url('./res/giphy.gif')";
+        socialBox.style.backgroundSize = "auto 400px";
+        socialBox.style.backgroundBlendMode = "darken";
+    }, 1000);
+
 }
 
 artBox.onclick = function() {
@@ -24,5 +35,10 @@ artBox.onclick = function() {
     artBox.style.backgroundImage = "url('./res/pinterest.jpg')";
     artBox.style.backgroundSize = "500px";
     artBox.style.backgroundBlendMode = "normal";
-    setTimeout(() => {  location.href = "https://www.pinterest.it/stefanodambrosio/"; }, 1000);
+    setTimeout(() => {  
+        location.href = "https://www.pinterest.it/stefanodambrosio/";
+        artBox.style.backgroundImage = "url('./res/pinterest.jpg')";
+        artBox.style.backgroundSize = "auto 400px";
+        artBox.style.backgroundBlendMode = "normal";
+    }, 1000);
 }
